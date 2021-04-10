ca54d5b7bd5fabaa5caa488f18aac08e9d93755f
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-04-09 22:33:11 -0700

    feat(CarscoreMainForm): Require each entry to be selected for the form to be valid

--- src/components/form/CarscoreMainForm.vue
+++ src/components/form/CarscoreMainForm.vue
@@ -1,24 +1,29 @@
 <template>
-  <v-container fluid>
-    <v-row align="center">
-      <v-col
-        v-for="entry in entries"
-        :key="entry.id"
-        class="d-flex"
-        cols="12"
-        sm="6"
-      >
-        <v-select
-          v-model="select[entry.id]"
-          :items="entry.items"
-          filled
-          :label="entry.label"
-          outlined
-        ></v-select>
-      </v-col>
-      <pre> {{ select }}</pre>
-    </v-row>
-  </v-container>
+  <v-form v-model="valid">
+    <v-container fluid>
+      <v-row align="center">
+        <v-col
+          v-for="entry in entries"
+          :key="entry.id"
+          class="d-flex"
+          cols="12"
+          sm="6"
+        >
+          <v-select
+            v-model="select[entry.id]"
+            :rules="RequireRule(entry.label)"
+            :label="entry.label"
+            :items="entry.items"
+            required
+            filled
+            outlined
+          ></v-select>
+        </v-col>
+        <pre> {{ select }}</pre>
+        <pre> {{ valid }}</pre>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
@@ -35,6 +40,8 @@ import attrs from "@/assets/data/carEvalAttrsSchema";
 
 export default {
   data: () => ({
+    valid: false,
+    // nameRules: [(v) => !!v || "Name is required"],
     select: {
       buying: "",
       maint: "",
@@ -47,6 +54,11 @@ export default {
   created() {
     // this.select = DEFAULT_SELECTION;
     this.entries = attrs;
+  },
+  methods: {
+    RequireRule(formEntry) {
+      return [(v) => !!v || `${formEntry} is required`];
+    },
   },
 };
 </script>
