541a866bd4ea4433ced30e6d7d10a6f38cc872d3
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-04-18 23:27:26 -0700

    refactor(MainNav): Move bar and drawer into their own components

---
+++ src/components/nav/MainAppBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app-bar app>
+    <v-img
+      lazy-src="@/assets/img/carscoreLazyLogo.png"
+      max-height="40"
+      max-width="40"
+      src="@/assets/img/carscoreMetalLogo.svg"
+    ></v-img>
+    <v-toolbar-title class="carscore_brand-font">Carscore</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn
+      fab
+      small
+      depressed
+      outlined
+      color="anchor"
+      class="mx-3"
+      @click="toggleDrawer"
+    >
+      <v-icon dark> mdi-format-list-bulleted-square </v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleDrawer() {
+      this.$emit("toggle-drawer");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/fonts/_fonts.scss';
+
+.carscore_brand-font {
+  @include fonts.logo_text;
+}
+</style>
--- src/components/nav/MainNav.vue
+++ src/components/nav/MainNav.vue
@@ -1,96 +1,32 @@
 <template>
   <span>
-    <!-- Must have the app property -->
-    <v-app-bar app>
-      <v-img
-        lazy-src="@/assets/img/carscoreLazyLogo.png"
-        max-height="40"
-        max-width="40"
-        src="@/assets/img/carscoreMetalLogo.svg"
-      ></v-img>
-      <v-toolbar-title class="carscore_brand-font">Carscore</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        fab
-        small
-        depressed
-        outlined
-        color="anchor"
-        class="mx-3"
-        @click="drawer = !drawer"
-      >
-        <v-icon dark> mdi-format-list-bulleted-square </v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>
-            <v-row align="center" justify="center">
-              <v-col cols="2" class="pr-0">
-                <v-img
-                  lazy-src="@/assets/img/cascoreLogoPlain.svg"
-                  max-height="20"
-                  max-width="20"
-                  src="@/assets/img/carscoreLazyLogo.png"
-                >
-                </v-img>
-              </v-col>
-              <v-col cols="10" class="pl-0">
-                <span class="carscore_brand-font"> Carscore</span>
-              </v-col>
-            </v-row>
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            ML-driven car score system
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <MainAppBar @toggle-drawer="toggleDrawer" />
+    <MainAppDrawer :openDrawer="openDrawer" @set-drawer="setDrawer" />
   </span>
 </template>
 
 <script>
+import MainAppBar from "./MainAppBar";
+import MainAppDrawer from "./MainAppDrawer";
+
 export default {
   name: "MainNav",
   components: {
-    // HeroBanner,
+    MainAppBar,
+    MainAppDrawer,
   },
   data: () => ({
-    drawer: false,
-    // group: null,
-    items: [
-      { title: "Settings", icon: "mdi-cog" },
-      { title: "About", icon: "mdi-help-box" },
-    ],
-    right: null,
+    openDrawer: false,
   }),
-  watch: {
-    // group() {
-    //   this.drawer = false;
-    // },
+  methods: {
+    toggleDrawer() {
+      this.openDrawer = !this.openDrawer;
+    },
+    setDrawer(drawer) {
+      this.openDrawer = drawer;
+    },
   },
 };
 </script>
 
-<style lang="scss">
-@use '@/styles/fonts/_fonts.scss';
 
-.carscore_brand-font {
-  @include fonts.logo_text;
-}
-</style>
