d4008b04e5532fa76923489a77fd0ba24eec1165
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-09-08 13:11:47 -0700

    Fix(train.js): Train on balanced dataset

--- server/api/services/scoring/neuralNet/train.js
+++ server/api/services/scoring/neuralNet/train.js
@@ -3,41 +3,65 @@ const dataFileName = "./data/carData.txt";
 const encoding = require("./encoding");
 const encode = require("./encode");
 
+const { shuffle } = require("lodash");
+
+const NUM_OF_FEATURES = 24;
+const NUM_OF_CLASSES = 4;
+
+function getRandSubarray(arr, n) {
+  const subArray = [];
+  const shuffledArray = shuffle(arr);
+  for (let i = 0; i < n; i++) {
+    subArray.push(shuffledArray[i]);
+  }
+  return subArray;
+}
+
+function defineModel() {
+  const model = tf.sequential();
+  model.add(
+    tf.layers.dense({
+      units: 7,
+      activation: "sigmoid",
+      inputShape: [NUM_OF_FEATURES],
+    })
+  );
+  model.add(
+    tf.layers.dense({
+      units: NUM_OF_CLASSES,
+      activation: "softmax",
+    })
+  );
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: tf.losses.softmaxCrossEntropy,
+    metrics: tf.metrics.categoricalAccuracy,
+  });
+  model.summary();
+  return model;
+}
+
 async function train(input, output) {
   try {
-    const NUM_OF_FEATURES = 24;
-    const NUM_OF_CLASSES = 4;
+    const model = defineModel();
 
-    const model = tf.sequential();
-    model.add(
-      tf.layers.dense({
-        units: 7,
-        activation: "sigmoid",
-        inputShape: [NUM_OF_FEATURES],
-      })
-    );
-    model.add(
-      tf.layers.dense({
-        units: NUM_OF_CLASSES,
-        activation: "softmax",
-      })
-    );
-    model.compile({
-      optimizer: tf.train.adam(),
-      loss: "meanSquaredError",
-      metrics: ["accuracy"],
-    });
-    model.summary();
+    const NUM_OF_PASSES_ON_BALANCED_SAMPLES = 10;
+    for (let j = 0; j < NUM_OF_PASSES_ON_BALANCED_SAMPLES; j++) {
+      const balanced_samples_xs = [];
+      const balanced_samples_ys = [];
+      for (let i = 0; i < NUM_OF_CLASSES; i++) {
+        balanced_samples_xs.push(getRandSubarray(input[i], 65));
+        balanced_samples_ys.push(getRandSubarray(output[i], 65));
+      }
+      const xs = tf.tensor2d(balanced_samples_xs.flat(), [65 * 4, 24]);
+      const ys = tf.tensor2d(balanced_samples_ys.flat(), [65 * 4, 4]);
+      const config = {
+        shuffle: true,
+        epochs: 100,
+      };
+      await model.fit(xs, ys, config);
+    }
 
-    const NUM_OF_SAMPLES = 1728;
-    const INPUT_SIZE = [NUM_OF_SAMPLES, NUM_OF_FEATURES];
-    const OUTPUT_SIZE = [NUM_OF_SAMPLES, NUM_OF_CLASSES];
-    const xs = tf.tensor2d(input, INPUT_SIZE);
-    const ys = tf.tensor2d(output, OUTPUT_SIZE);
-
-    const epochs = 1000;
-    await model.fit(xs, ys, { epochs });
-    
     const MODEL_DIRECTORY = "file://./carscoreNN";
     await model.save(MODEL_DIRECTORY);
   } catch (error) {
@@ -46,4 +70,21 @@ async function train(input, output) {
 }
 
 const { input, output } = encode(dataFileName, encoding);
-train(input, output);
+
+console.log("input", input);
+console.log("output", output);
+
+const input_class_buckets = [[], [], [], []];
+const output_class_buckets = [[], [], [], []];
+
+output.forEach((out, i) => {
+  const index_class = out.indexOf(1);
+  input_class_buckets[index_class].push(input[i]);
+  output_class_buckets[index_class].push(output[i]);
+});
+
+// console.log(input_class_buckets);
+console.log(input_class_buckets.length);
+console.log(input_class_buckets[0].length);
+
+train(input_class_buckets, output_class_buckets);
