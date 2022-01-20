ec1c3b6e32ab20c63cf96e2e3f4c2b5ac220ab9d
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-01-19 16:59:48 -0800

    feat(scoring): Comment out console.log score.

--- client/src/store/modules/scoring.js
+++ client/src/store/modules/scoring.js
@@ -1,49 +1,47 @@
 import axios from "axios";
 
-import { SCORE_STATE } from '@/store/state-types.js';
-import { SET_SCORE } from '@/store/mutation-types.js';
-import { FETCH_SCORE } from '@/store/action-types.js';
+import { SCORE_STATE } from "@/store/state-types.js";
+import { SET_SCORE } from "@/store/mutation-types.js";
+import { FETCH_SCORE } from "@/store/action-types.js";
 
-import { SET_CAR } from '@/store/mutation-types.js';
+import { SET_CAR } from "@/store/mutation-types.js";
 
 const INIT_CAR = Object.freeze({
-    buying: "",
-    maint: "",
-    doors: "",
-    persons: "",
-    lug_boot: "",
-    safety: "",
+  buying: "",
+  maint: "",
+  doors: "",
+  persons: "",
+  lug_boot: "",
+  safety: "",
 });
 
 export default {
-    state: {
-        car: { ...INIT_CAR },
-        score: SCORE_STATE[0],
+  state: {
+    car: { ...INIT_CAR },
+    score: SCORE_STATE[0],
+  },
+  getters: {
+    initCar: () => ({ ...INIT_CAR }),
+    car: (state) => state.car,
+    score: (state) => state.score,
+  },
+  mutations: {
+    [SET_CAR]: (state, payload) => (state.car = { ...state.car, ...payload }),
+    [SET_SCORE]: (state, payload) => (state.score = SCORE_STATE[payload]),
+  },
+  actions: {
+    [FETCH_SCORE]: async ({ commit }, payload) => {
+      try {
+        // console.log(getters.car)
+        console.log(payload);
+        const URL = "api/score";
+        const data = { car: payload };
+        const { score } = await (await axios.post(URL, data)).data;
+        // console.log('score', score)
+        commit(SET_SCORE, score);
+      } catch (error) {
+        console.log(error);
+      }
     },
-    getters: {
-        initCar: () => ({ ...INIT_CAR }),
-        car: (state) => state.car,
-        score: (state) => state.score,
-    },
-    mutations: {
-        [SET_CAR]: (state, payload) =>
-            state.car = { ...state.car, ...payload },
-        [SET_SCORE]: (state, payload) =>
-            state.score = SCORE_STATE[payload],
-    },
-    actions: {
-        [FETCH_SCORE]: async ({ commit }, payload) => {
-            try {
-                // console.log(getters.car)
-                console.log(payload)
-                const URL = 'api/score'
-                const data = { car: payload };
-                const { score } = await (await axios.post(URL, data)).data;
-                console.log('score', score)
-                commit(SET_SCORE, score);
-            } catch (error) {
-                console.log(error);
-            }
-        },
-    }
-}
+  },
+};
