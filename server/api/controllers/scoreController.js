ef86f37f48d2d1457567d4bdbdd55f9d52e04d07
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-02-11 01:19:05 -0800

    refactor(scoreController): Removed unneeded logs.

--- server/api/controllers/scoreController.js
+++ server/api/controllers/scoreController.js
@@ -1,9 +1,7 @@
 const ScoringService = require("../services/scoring/ScoringService");
 
 exports.score_detail = async (req, res) => {
-    console.log("I'm at score detail")
     const { car } = req.body;
-    console.log(car);
     const score = await ScoringService.scoreOne(car);
     res.status(200).json({ score });
 };
