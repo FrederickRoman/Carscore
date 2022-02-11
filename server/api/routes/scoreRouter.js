f7c0237696251bf4615958758d73eb075731755f
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-02-11 01:20:54 -0800

    refactor(scoreRouter): Removed unneeded comments.

--- server/api/routes/scoreRouter.js
+++ server/api/routes/scoreRouter.js
@@ -1,14 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const score_controller = require('../controllers/scoreController');
+const score_controller = require("../controllers/scoreController");
 
-// const urlencodedParser = express.urlencoded({ extended: false });
-// var bodyParser = require('body-parser')
-
-// const bodyParser = require('body-parser')
-
-router.route('/')
-    .all(express.json())
-    .post(score_controller.score_detail);
+router.route("/").all(express.json()).post(score_controller.score_detail);
 
 module.exports = router;
